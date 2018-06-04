49b549e9caa183b461ff4313800646b6eb41bdb4
psvm <38030098+psvm@users.noreply.github.com>
2018-06-04 16:00:20 +0300

    Code was refactored

--- fraction.py
+++ fraction.py
@@ -4,22 +4,22 @@ import copy
 import fractions
 
 
-def find_three_max(sequence_of_fractions):
+def find_three_max(sequence):
     indexes_of_max_elements = []
     values_of_max_elements = []
 
     for i in range(0, 3):
-        values_of_max_elements.append(max(sequence_of_fractions))
-        indexes_of_max_elements.append(sequence_of_fractions.index(values_of_max_elements[i]))
-        sequence_of_fractions.remove(values_of_max_elements[i])
+        values_of_max_elements.append(max(sequence))
+        indexes_of_max_elements.append(sequence.index(values_of_max_elements[i]))
+        sequence.remove(values_of_max_elements[i])
 
     for i in range(0, 3):
-        sequence_of_fractions.insert(indexes_of_max_elements[i], values_of_max_elements[i])
+        sequence.insert(indexes_of_max_elements[i], values_of_max_elements[i])
 
     half_sum = sum(values_of_max_elements) / 2
     for i in reversed(sorted(indexes_of_max_elements)):
-        sequence_of_fractions.insert(i + 1, half_sum)
-    return sequence_of_fractions
+        sequence.insert(i + 1, half_sum)
+    return sequence
 
 
 sequence_of_fractions = [fractions.Fraction(4, 5), fractions.Fraction(1, 3),
